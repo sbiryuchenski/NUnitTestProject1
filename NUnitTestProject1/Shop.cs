02a4c6ab3dc75a9f433c46086679d32c78e3d8f1
Sergey <serjjjoyl@gmail.com>
2021-02-20 15:14:23 +0300

    магазин

---
+++ NUnitTestProject1/Shop.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System.Diagnostics;
+using System.IO;
+using OpenQA.Selenium.Interactions;
+
+
+namespace NUnitTestProject1
+{
+    [TestFixture]
+    public class Shop:BaseTest
+    {
+        
+        [Test, Order(1)]
+        public void CheckBooks()
+        {
+            SetTab("Books");
+            Check("/books");
+        }
+        [Test, Order(2)]
+        public void CheckComputers()
+        {
+            SetTab("Computers");
+            Check("/computers");
+        }
+        [Test, Order(3)]
+        public void CheckElectronics()
+        {
+            SetTab("Electronics");
+            Check("/electronics");
+        }
+        [Test, Order(4)]
+        public void CheckApparelnShoes()
+        {
+            SetTab("Apparel & Shoes");
+            Check("/apparel-shoes");
+        }
+        [Test, Order(5)]
+        public void CheckDigitalDownloads()
+        {
+            SetTab("Digital downloads");
+            Check("/digital-downloads");
+        }
+        [Test, Order(6)]
+        public void CheckJewelry()
+        {
+            SetTab("Jewelry");
+            Check("/jewelry");
+        }
+        [Test, Order(7)]
+        public void CheckGiftCards()
+        {
+            SetTab("Gift Cards");
+            Check("/gift-cards");
+        }
+        [Test, Order(8)]
+        public void CheckSubDesktops()
+        {
+            MoveOnTab("Computers");
+            SetTab("Desktops");
+            Check("/desktops");
+        }
+        [Test, Order(9)]
+        public void CheckSubNotebooks()
+        {
+            MoveOnTab("Computers");
+            SetTab("Notebooks");
+            Check("/notebooks");
+        }
+        [Test, Order(10)]
+        public void CheckcSubAccessories()
+        {
+            MoveOnTab("Computers");
+            SetTab("Accessories");
+            Check("/accessories");
+        }
+        [Test, Order(11)]
+        public void CheckSubCameraPhoto()
+        {
+            MoveOnTab("Electronics");
+            SetTab("Camera, photo");
+            Check("/camera-photo");
+        }
+        [Test, Order(12)]
+        public void CheckSubCellPhones()
+        {
+            MoveOnTab("Electronics");
+            SetTab("Cell phones");
+            Check("/cell-phones");
+        }
+
+    }
+}
--- NUnitTestProject1/UnitTest1.cs
+++ NUnitTestProject1/UnitTest1.cs
@@ -5,6 +5,7 @@ using System.Diagnostics;
 using System.IO;
 using System.Text;
 using OpenQA.Selenium.Interactions;
+using System;
 
 namespace NUnitTestProject1
 {
@@ -12,40 +13,44 @@ namespace NUnitTestProject1
     public class BaseTest
     {
         protected IWebDriver driver;
-        protected IWebElement search;
-        protected IWebElement button;
-        protected IAction enter;
+        protected IWebElement tab;
+        protected IWebElement movetab;
+
 
         [OneTimeSetUp]
         public void Initialization()
         {
             string path = Directory.GetCurrentDirectory();
             driver = new ChromeDriver(path);
-        }
-        [SetUp, Order(2)]
-        virtual public void FindOnPage()
-        {
-            search = driver.FindElement(By.XPath("//input[@name = 'q']"));
+            
         }
         [OneTimeSetUp, Order(1)]
         virtual public void SetURL()
         {
-            driver.Url = "http://www.google.com";
+            driver.Url = "http://demowebshop.tricentis.com/";
+        }
+        virtual protected void SetTab(string i)
+        {   
+            tab = driver.FindElement(By.XPath("//a[normalize-space(text())='" + i + "']"));
+            tab.Click();
+        }
+        virtual protected void MoveOnTab(string i)
+        {
+            movetab = driver.FindElement(By.XPath("//a[normalize-space(text())='" + i + "']"));
+            Actions move = new Actions(driver);
+            move.MoveToElement(movetab).Build().Perform();
         }
 
-        virtual public void FindButton()
+        virtual protected void Check(string check)
         {
-            button = driver.FindElement(By.XPath("//button[@type = 'submit']"));
-        }
-        virtual public bool Check()
-        {
-            return(driver.FindElement(By.CssSelector("#result-stats")).Displayed);
+            string url = driver.Url;
+            Assert.IsTrue(url.Contains(check), "URL не совпадает с ожидаемым");
         }
 
         [OneTimeTearDown]
         public void CloseBrowser()
         {
             driver.Close();
-        }    
+        }
     }
 }
