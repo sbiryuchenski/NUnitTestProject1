e793c06a171c0cab95461f5a847ddcc654b2f16a
Serg <serjjjoy@gmail.com>
2021-04-21 12:36:26 +0300

    Перетягивание элемента

---
+++ NUnitTestProject1/TestElements.cs
@@ -0,0 +1,45 @@
+﻿using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.IE;
+using OpenQA.Selenium.Firefox;
+using OpenQA.Selenium.Edge;
+using System.Diagnostics;
+using System.IO;
+using System.Text;
+using OpenQA.Selenium.Interactions;
+using System;
+using OpenQA.Selenium.Support.UI;
+using System.Configuration;
+using System.Text.Json;
+using Microsoft.Extensions.Configuration;
+using System.Threading;
+
+namespace NUnitTestProject1
+{
+    [TestFixture]
+    class TestElements:BaseTest
+    {
+        public override void SetURL()
+        {
+            driver.Url = "https://www.w3schools.com/howto/howto_js_draggable.asp";
+        }
+        [Test]
+        public void Test1()
+        {
+            IWebElement dragelement = driver.FindByXpath("//div[@id='mydivheader']");
+            IWebElement point1 = driver.FindByXpath("//a[@class='w3-left w3-btn']");
+            IWebElement point2 = driver.FindByXpath("//a[@class='w3-right w3-btn']");
+            IWebElement point3 = driver.FindByXpath("//a[@class='w3schools-logo notranslate']");
+            Actions move = new Actions(driver);
+            move.ClickAndHold(dragelement).MoveToElement(point1).Release(dragelement).Perform();
+            Thread.Sleep(100);
+            move.ClickAndHold(dragelement).MoveToElement(point2).Release(dragelement).Perform();
+            Thread.Sleep(100);
+            move.ClickAndHold(dragelement).MoveToElement(point3).Release(dragelement).Perform();
+            Thread.Sleep(100);
+
+        }
+
+    }
+}
