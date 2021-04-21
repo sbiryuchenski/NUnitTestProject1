c6d2f85eac933ca441f0a7ec85cffb773a0ecaf7
Serg <serjjjoy@gmail.com>
2021-04-21 18:04:12 +0300

    добавил js скрипты и ещё что-то вроде

--- NUnitTestProject1/TestElements.cs
+++ NUnitTestProject1/TestElements.cs
@@ -31,15 +31,36 @@ namespace NUnitTestProject1
             IWebElement point1 = driver.FindByXpath("//a[@class='w3-left w3-btn']");
             IWebElement point2 = driver.FindByXpath("//a[@class='w3-right w3-btn']");
             IWebElement point3 = driver.FindByXpath("//a[@class='w3schools-logo notranslate']");
-            Actions move = new Actions(driver);
-            move.ClickAndHold(dragelement).MoveToElement(point1).Release(dragelement).Perform();
-            Thread.Sleep(100);
-            move.ClickAndHold(dragelement).MoveToElement(point2).Release(dragelement).Perform();
-            Thread.Sleep(100);
-            move.ClickAndHold(dragelement).MoveToElement(point3).Release(dragelement).Perform();
-            Thread.Sleep(100);
+            // Actions move = new Actions(driver);
+            // Первый вариант
+            /* move.ClickAndHold(dragelement).Perform();
+             move.MoveToElement(point1).Perform(); 
+             move.Release(dragelement).Perform();
+             Thread.Sleep(100);
+             move.ClickAndHold(dragelement).Perform();
+             move.MoveToElement(point2).Perform();
+             move.Release(dragelement).Perform();
+             Thread.Sleep(100);
+             move.ClickAndHold(dragelement).Perform();
+             move.MoveToElement(point3).Perform();
+             move.Release(dragelement).Perform();
+             Thread.Sleep(100);
 
+             // Второй вариант
+             move.DragAndDrop(dragelement, point1).Build().Perform();
+             Thread.Sleep(100);
+             move.DragAndDrop(dragelement, point2).Build().Perform();
+             Thread.Sleep(100);
+             move.DragAndDrop(dragelement, point3).Build().Perform();
+             Thread.Sleep(100);
+             #region
+             // Тут ничего нет
+             #endregion
+            */
+            driver.JSDragDrop("//div[@id='mydivheader']", "//a[@class='w3-left w3-btn']");
+            Thread.Sleep(100);
         }
+           
 
     }
 }
